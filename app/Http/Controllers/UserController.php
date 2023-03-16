62e686f3c5f87d9809db6ab1a3f145732885a6d0
hitesh1696 <hitesh.patil1696@gmail.com>
2023-03-16 17:49:33 +0530

    difficulties in handling data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,6 @@ class UserController extends Controller
 {
     public function store_user(Request $request)
     {
-        // $d = $request->();
-        //  $d = json_decode($request);
         return $request->params;
     }
 }

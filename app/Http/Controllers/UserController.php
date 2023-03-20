8f9a54e130aa5bedb58d71c253fbf680551ea719
hitesh1696 <hitesh.patil1696@gmail.com>
2023-03-20 11:53:13 +0530

    Mobile validation done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,9 @@ class UserController extends Controller
     {
         $validator = Validator::make($request->all(),[
             'params.mobile' => 'required|regex:/^([0-9\s\-\+\(\)]*)$/|min:10|max:11',
+        ],[
+            'params.mobile' => 'The mobile number is required.',
+            'params.mobile.regex' => 'The mobile field should contain only numbers,and it should be in-between 10-11',            
         ]);
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()]);
@@ -27,41 +30,54 @@ class UserController extends Controller
     }
     public function store_user(Request $request)
     {
-        // return $request;
         $validator = Validator::make($request->all(),[
-            'params.mobile' => 'regex:/^([0-9\s\-\+\(\)]*)$/|min:10|max:20',
             'params.email' => 'required|email',
             'params.name' => 'required|string',
             'params.terms_conditions' => 'required|boolean',
+        ],[
+            'params.email' => 'The email is required',
+            'params.email.email' => 'The email should be valid email-id',
+            'params.name' => 'The name is required',
         ]);
+        return 1;
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()]);
         } else {
-            $users = User::where('phone_number',$request->input('params.mobile'))->get();
-        if(isEmpty($users))
-        {
-            $user = User::create([
-                "name" => $validator['params']['name'],
-                "phone_number_country_code" => $request->input('country_code'),
-                "phone_number" => $validator['params']['mobile'],
-                "email" => $validator['params']['email'],
-                "terms_conditions" => $validator['params']['terms_conditions'],
-            ]);
-            return response()->json([
-                'flag' => 1,
-                'msg' => "Welcome To Dwebpixel Club " . $user->name,
-            ]);
+            $users = User::where('phone_number', $request->input('params.mobile'))->get();
+            if(!$users)
+            {
+                $user = User::create([
+                    "name" =>$request->input('params.name'),
+                    "phone_number_country_code" => $request->input('params.country_code'),
+                    "phone_number" => $request->input('params.mobile'),
+                    "email" =>$request->input('params.email'),
+                    "terms_conditions" =>$request->input('params.terms_conditions'),
+                ]);
+
+                if($user)
+                {
+                    return response()->json([
+                        'flag' => 1,
+                        'msg' => "Welcome To Dwebpixel Club ",
+                        'errors' => null
+                    ]);
+                } 
+                else {
+                    return response()->json([
+                        'flag' => 1,
+                        'msg' => "Something went wrong ",
+                    ]);
+                }
+                
+            }
+            else {
+                return response()->json([
+                    'flag' => 2,
+                    'msg' => "Mobile Number already exists, you can login with this number or try different number",
+                ]);
+                
+            }
         }
-        else {
-            return response()->json([
-                'flag' => 2,
-                'msg' => "Mobile Number already exists, you can login with this number or try different number",
-            ]);
-            
-        }
-        }
-        
-        // $email = $request->input('params.email');
-        // return $request->params;
     }
+
 }
